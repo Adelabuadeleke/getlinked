79e5dd5cce6ed1e987de071f8f3735b85a202052
kamal <kamade123@gmail.com>
2023-09-21 22:48:11 +0100

    added register & contact routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-// pages
-import Home from './pages/Home';
 import { 
   Route,
   BrowserRouter as Router,
   Routes,
 
 } from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Register from './pages/Register';
+
 
 
 function App() {
@@ -20,6 +23,8 @@ function App() {
      <Router>
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/register' element={<Register />}/>
       </Routes>
      </Router>
     </>
