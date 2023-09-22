1972a4447def0ce1538aca02c38bfb82a700e405
kamal <kamade123@gmail.com>
2023-09-22 17:48:54 +0100

    added sidebar to nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,68 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../styles/nav.css'
 
 function Nav() {
-  return (
-    <div className='nav'>
-     <div className="nav_first">
-      <h2>get<span className="purple">linked</span></h2>
-     </div>
-     <div className="nav_second">
-      <ul>
-       <li>Timeine</li>
-       <li>Overview</li>
-       <li>FAQs</li>
-       <li>Contact</li>
-      </ul>
+   useEffect(()=>{
+    const sidebar = document.querySelector('.sidebar')
+    const menuBtn = document.querySelector('.menu')
+    const closeBtn = document.querySelector('.sidebar_register_cancel')
+    const body = document.querySelector('body')
 
-      <button className="register">
-       Register
-      </button>
-     </div>
+
+    menuBtn.addEventListener('click', ()=>{
+      sidebar.classList.add('show_toggle')
+      body.classList.add('stop-scrolling')
+    })
+
+    closeBtn.addEventListener('click', ()=> {
+      sidebar.classList.remove('show_toggle')
+      body.classList.remove('stop-scrolling')
+    })
+  })
+  return (
+    <div className='nav_all'>
+      <nav className="nav">
+        <div className="nav_first">
+          <h2>get<span className="purple">linked</span></h2>
+        </div>
+
+        <div className="mobile_toggle">
+          <img className='menu' src="../../imgs/nav_toggle.svg" alt="" />
+        </div>
+        <div className="nav_second">
+          <ul>
+            <li>Timeline</li>
+            <li>Overview</li>
+            <li>FAQs</li>
+            <li>Contact</li>
+          </ul>
+
+          <button className="register">
+            Register
+          </button>
+        </div>
+      </nav>
+
+      <aside className='sidebar'>
+        <div className="sidebar_inner">
+          <div className="sidebar_top">
+            <button className="sidebar_register_cancel">
+              X
+            </button>
+          </div>
+          <ul>
+          <a href=""><li>Timeine</li></a>
+          <a href=""><li>Overview</li></a>
+          <a href=""><li>FAQs</li></a>
+          <a href=""><li>Contact</li></a>
+          </ul>
+
+          <button className="register">
+            Register
+          </button>
+        </div>
+      </aside>
+     
     </div>
   )
 }
