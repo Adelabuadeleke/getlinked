aa26ae8f186d1456a1b0f962c39445fc1051380a
kamal <kamade123@gmail.com>
2023-09-23 05:23:38 +0100

    connected register page to api

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import Nav from '../components/Nav';
 import '../styles/register.css';
+import axios from '../axios';
+import requests from '../requests';
+
 
 function Register() {
+ const [name, setName] = useState('')
+ const [phone, setPhone] = useState('')
+ const [email, setEmail] = useState('')
+ const [topic, setTopic] = useState('')
+ const [category, setCatergory] = useState('')
+ const [size, setSize] = useState('')
+
+ const sendRegistration = async ()=>{
+ 
+  const details ={
+   name:name,
+   phone:name,
+   email: email,
+   topic: topic,
+   category: category,
+   size: size
+  }
+
+  const result = await axios.post(requests.fetchRegister, details)
+  .then(result=>{
+   console.log(result)
+  })
+  console.log('registration sucessful!')
+}
   return (
     <div className='register_div'>
      <Nav />
@@ -24,31 +51,56 @@ function Register() {
         <div className="input_div mobile_flex">
          <div className="input_div_item">
           <label htmlFor="">Team's Name</label>
-          <input type="text" placeholder='Enter the name of your group'/>
+          <input 
+          type="text" 
+          placeholder='Enter the name of your group'
+          value={name}  
+          onChange={e => setName(e.target.value)}
+          />
          </div>
 
          <div className="input_div_item">
           <label htmlFor="">Phone</label>
-          <input type="text" placeholder='Enter your phone number'/>
+          <input 
+          type="text" 
+          placeholder='Enter your phone number'
+          value={phone}  
+          onChange={e => setPhone(e.target.value)}
+          />
          </div>
         </div>
 
         <div className="input_div mobile_flex">
          <div className="input_div_item">
           <label htmlFor="">Email</label>
-          <input type="text" placeholder='Enter your email address'/>
+          <input 
+          type="email" 
+          placeholder='Enter your email address'
+          value={email}  
+          onChange={e => setEmail(e.target.value)}
+          />
          </div>
 
          <div className="input_div_item">
           <label htmlFor="">Project Topic</label>
-          <input type="text" placeholder='What is your group project topic'/>
+          <input 
+          type="text" 
+          placeholder='What is your group project topic'
+          value={topic}  
+          onChange={e => setTopic(e.target.value)}
+          />
          </div>
         </div>
 
         <div className="input_div_flex">
          <div className="input_div_item">
           <label htmlFor="">Category</label>
-          <select className='category' name="" id="">
+          <select 
+           value={category}  
+           onChange={e => setCatergory(e.target.value)}
+           className='category' 
+           name="" 
+           id="">
            <option value=""> select your category</option>
            <option value="one">one</option>
            <option value="two">two</option>
@@ -58,7 +110,12 @@ function Register() {
 
          <div className="input_div_item">
           <label htmlFor="">Group Size</label>
-          <select className='group' name="" id="">
+          <select 
+            value={size}  
+           onChange={e => setSize(e.target.value)}
+           className='group' 
+           name="" 
+           id="">
            <option >select</option>
            <option value="one">one</option>
            <option value="two">two</option>
@@ -73,7 +130,7 @@ function Register() {
         <input type="checkbox" name="" id="" />I agreed with the event terms and conditions  and privacy policy
        </div>
 
-       <button className="register_now">
+       <button className="register_now" onClick={sendRegistration}>
         Register Now
        </button>
        </form>
