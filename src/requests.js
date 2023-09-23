5e00dbec6c813782284704ba93620e70a95f3ed2
kamal <kamade123@gmail.com>
2023-09-23 05:26:20 +0100

    added requests

---
+++ src/requests.js
@@ -0,0 +1,7 @@
+const requests =  {
+ fetchRegister:'/hackathon/registration',
+ fetchContact:'/hackathon/contact-form',
+ fetchCategoriesList:'/hackathon/categories-list'
+}
+
+export default requests;
